95db5965009b0a362bf2e1da8539e358ae90dbc0
Brian Wright <bw@shadowdragon.io>
2022-10-05 23:05:55 -0500

    remove padding from location component

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -37,6 +37,12 @@ local diagnostics = {
   }
 }
 
+-- location component
+local location = {
+  "location",
+  padding = 0,
+}
+
 lualine.setup({
   options = {
     icons_enabled = true,
@@ -52,13 +58,13 @@ lualine.setup({
     lualine_c = { "filename" },
     lualine_x = { "encoding", "fileformat", "filetype" },
     lualine_y = { "progress" },
-    lualine_z = { "location" },
+    lualine_z = { location },
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
     lualine_c = { "filename" },
-    lualine_x = { "location" },
+    lualine_x = { location },
     lualine_y = {},
     lualine_z = {},
   },
