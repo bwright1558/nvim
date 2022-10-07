ceebadc0d747f8397193e46c7a4db321442cd541
Brian Wright <bw@shadowdragon.io>
2022-10-07 10:50:41 -0500

    add descriptions to user commands

--- lua/user/commands.lua
+++ lua/user/commands.lua
@@ -1,5 +1,5 @@
 local command = vim.api.nvim_create_user_command
 
-command("TrimWhitespace", "%s/\\s\\+$//e", {})
-command("TrimNewlines", "%s/\\($\\n\\s*\\)\\+\\%$//e", {})
-command("Format", vim.lsp.buf.formatting_seq_sync, {})
+command("TrimWhitespace", "%s/\\s\\+$//e", { desc = "Trim trailing whitespace" })
+command("TrimNewlines", "%s/\\($\\n\\s*\\)\\+\\%$//e", { desc = "Trim trailing newlines" })
+command("Format", vim.lsp.buf.formatting_seq_sync, { desc = "Format current buffer with LSP" }) -- XXX: Maybe put in lsp on_attach function?
