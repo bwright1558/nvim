5d7a2192af7acad5e27bd64988a6d61751829613
Brian Wright <bw@shadowdragon.io>
2022-10-07 22:30:34 -0500

    use icon for indentline

--- lua/user/configs/indentline.lua
+++ lua/user/configs/indentline.lua
@@ -3,6 +3,8 @@ if not ok then
   return
 end
 
+local icons = require("user.icons")
+
 indent_blankline.setup({
   buftype_exclude = { "terminal", "nofile", "nowrite" },
   filetype_exclude = {
@@ -13,7 +15,7 @@ indent_blankline.setup({
     "Trouble",
     "text",
   },
-  char = "│",
+  char = icons.ui.LineMiddle,
   show_trailing_blankline_indent = false,
   show_first_indent_level = true,
   use_treesitter = true,
