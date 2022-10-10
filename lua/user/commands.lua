986ef2c13a58e9ced44792eb17567cb85380ca2e
Brian Wright <bw@shadowdragon.io>
2022-10-09 23:57:17 -0500

    fix format command

--- lua/user/commands.lua
+++ lua/user/commands.lua
@@ -2,4 +2,4 @@ local command = vim.api.nvim_create_user_command
 
 command("TrimWhitespace", "%s/\\s\\+$//e", { desc = "Trim trailing whitespace" })
 command("TrimNewlines", "%s/\\($\\n\\s*\\)\\+\\%$//e", { desc = "Trim trailing newlines" })
-command("Format", vim.lsp.buf.format, { desc = "Format" })
+command("Format", function() vim.lsp.buf.format() end, { desc = "Format" })
