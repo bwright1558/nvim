41f0c644549680be0453659881112f91ae910931
Brian Wright <bw@shadowdragon.io>
2023-02-05 23:23:50 -0600

    wrap requires in parens

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
-require "user.options"
-require "user.lazy"
-require "user.commands"
-require "user.autocmds"
-require "user.keymaps"
+require("user.options")
+require("user.lazy")
+require("user.commands")
+require("user.autocmds")
+require("user.keymaps")
