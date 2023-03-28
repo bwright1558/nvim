59b937283202cca240f870f2921c498c1956e8ff
Brian Wright <bw@shadowdragon.io>
2023-03-27 23:05:40 -0500

    only make these clipboard changes on gnome DE

--- lua/user/clipboard.lua
+++ lua/user/clipboard.lua
@@ -3,7 +3,7 @@
 -- that results in a copy/paste operation to occur.
 -- To get around this, we use xclip (X11 tool) as an alternative, but this only works
 -- well if XWayland is running. Luckily for me, it is running (on Fedora Linux).
-if vim.fn.empty(vim.env.WAYLAND_DISPLAY) == 0 and vim.fn.executable("xclip") == 1 then
+if vim.fn.empty(vim.env.WAYLAND_DISPLAY) == 0 and vim.env.DESKTOP_SESSION == "gnome" and vim.fn.executable("xclip") == 1 then
   vim.g.clipboard = {
     name = "xclip",
     copy = {
