626d2655c449c06e746eecd639d5b46e2e403130
Brian Wright <bw@shadowdragon.io>
2023-04-07 09:34:12 -0500

    "fix" colorscheme transparency

--- lua/user/plugins/colorscheme/catppuccin.lua
+++ lua/user/plugins/colorscheme/catppuccin.lua
@@ -6,19 +6,25 @@ local M = {
 function M.config()
   require("catppuccin").setup({
     flavour = "mocha", -- latte, frappe, macchiato, mocha
-    transparent_background = true,
     custom_highlights = function(C)
+      local transparent_bg = { fg = C.text, bg = C.none }
       local normal_float = { fg = C.text, bg = C.mantle }
       local float_border = { fg = C.blue, bg = C.mantle }
 
       return {
+        -- Manually set transparent background because
+        -- Catppuccin's method (transparent_background option)
+        -- will set transparency on elements that don't need
+        -- transparency, e.g. lualine.
+        Normal = transparent_bg,
+        NormalNC = transparent_bg,
+        -- Custom highlights on specific components.
         NormalFloat = normal_float,
         FloatBorder = float_border,
         TelescopeNormal = normal_float,
         TelescopeBorder = float_border,
         BqfPreviewFloat = normal_float,
         BqfPreviewBorder = float_border,
-        -- NvimTreeNormal = normal_float,
       }
     end,
   })
