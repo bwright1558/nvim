8c1a50468cccdf335fa2aafdd38179918d5a70a7
Brian Wright <bw@shadowdragon.io>
2024-06-25 14:18:05 -0500

    fix outdated gitsigns config

--- lua/user/plugins/gitsigns.lua
+++ lua/user/plugins/gitsigns.lua
@@ -9,46 +9,16 @@ function M.config()
 
   require("gitsigns").setup({
     signs = {
-      add = {
-        hl = "GitSignsAdd",
-        text = icons.ui.BoldLineLeft,
-        numhl = "GitSignsAddNr",
-        linehl = "GitSignsAddLn",
-      },
-      change = {
-        hl = "GitSignsChange",
-        text = icons.ui.BoldLineLeft,
-        numhl = "GitSignsChangeNr",
-        linehl = "GitSignsChangeLn",
-      },
-      delete = {
-        hl = "GitSignsDelete",
-        text = icons.ui.Triangle,
-        numhl = "GitSignsDeleteNr",
-        linehl = "GitSignsDeleteLn",
-      },
-      topdelete = {
-        hl = "GitSignsDelete",
-        text = icons.ui.Triangle,
-        numhl = "GitSignsDeleteNr",
-        linehl = "GitSignsDeleteLn",
-      },
-      changedelete = {
-        hl = "GitSignsChange",
-        text = icons.ui.BoldLineLeft,
-        numhl = "GitSignsChangeNr",
-        linehl = "GitSignsChangeLn",
-      },
-      untracked = {
-        hl = "GitSignsAdd",
-        text = icons.ui.BoldLineDotted,
-        numhl = "GitSignsAddNr",
-        linehl = "GitSignsAddLn",
-      },
+      add = { text = icons.ui.BoldLineLeft },
+      change = { text = icons.ui.BoldLineLeft },
+      delete = { text = icons.ui.Triangle },
+      topdelete = { text = icons.ui.Triangle },
+      changedelete = { text = icons.ui.BoldLineLeft },
+      untracked = { text = icons.ui.BoldLineDotted },
     },
     signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-    numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
-    linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+    numhl = false,     -- Toggle with `:Gitsigns toggle_numhl`
+    linehl = false,    -- Toggle with `:Gitsigns toggle_linehl`
     word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
     watch_gitdir = {
       interval = 1000,
@@ -62,12 +32,9 @@ function M.config()
       delay = 1000,
       ignore_whitespace = false,
     },
-    current_line_blame_formatter_opts = {
-      relative_time = false,
-    },
     sign_priority = 6,
     update_debounce = 100,
-    status_formatter = nil, -- Use default
+    status_formatter = nil,  -- Use default
     max_file_length = 50000, -- Disable if file is longer than this (in lines)
     preview_config = {
       -- Options passed to nvim_open_win
@@ -76,9 +43,6 @@ function M.config()
       relative = "cursor",
       row = 0,
       col = 1,
-    },
-    yadm = {
-      enable = false,
     },
   })
 end
