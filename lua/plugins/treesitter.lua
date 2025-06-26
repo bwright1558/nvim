b41ee900aafa2f1cdd737c7f18f38116dcbe9b16
Brian Wright <bw@shadowdragon.io>
2025-06-25 21:21:33 -0500

    Add treesitter textobjects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,6 +8,9 @@
 
 local M = {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
   branch = "master",
   lazy = false,
   build = ":TSUpdate",
@@ -38,6 +41,47 @@ local M = {
         node_incremental = "<CR>",   -- Expand to next node
         scope_incremental = "<Tab>", -- Expand to next scope
         node_decremental = "<BS>",   -- Shrink selection
+      },
+    },
+
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true, -- Jump forward to textobj, like `]]`
+
+        keymaps = {
+          -- Use `a` for around and `i` for inner
+          ["af"] = { query = "@function.outer", desc = "Select outer part of a function region" },
+          ["if"] = { query = "@function.inner", desc = "Select inner part of a function region" },
+          ["ac"] = { query = "@class.outer", desc = "Select outer part of a class region" },
+          ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+          ["aa"] = { query = "@parameter.outer", desc = "Select outer part of a parameter region" },
+          ["ia"] = { query = "@parameter.inner", desc = "Select inner part of a parameter region" },
+          ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
+        },
+      },
+
+      move = {
+        enable = true,
+        set_jumps = true,
+        goto_next_start = {
+          ["]m"] = "@function.outer",
+          ["]c"] = "@class.outer",
+        },
+        goto_previous_start = {
+          ["[m"] = "@function.outer",
+          ["[c"] = "@class.outer",
+        },
+      },
+
+      swap = {
+        enable = true,
+        swap_next = {
+          ["<Leader>a"] = "@parameter.inner",
+        },
+        swap_previous = {
+          ["<Leader>A"] = "@parameter.inner",
+        },
       },
     },
   },
