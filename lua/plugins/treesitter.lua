1d81ab311fa5997470510b4324b99383a40670ac
Brian Wright <bw@shadowdragon.io>
2025-06-25 20:37:13 -0500

    Add base treesitter plugin config

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,38 @@
+local M = {
+  "nvim-treesitter/nvim-treesitter",
+  branch = "master",
+  lazy = false,
+  build = ":TSUpdate",
+
+  -- Uses the plugin's built-in setup function
+  main = "nvim-treesitter.configs",
+
+  opts = {
+    -- Install all available maintained parsers,
+    -- except for those explicitly ignored
+    ensure_installed = "all",
+    ignore_install = {
+      "norg", -- causes build issues on some systems
+    },
+
+    highlight = {
+      enable = true,
+    },
+
+    indent = {
+      enable = true,
+    },
+
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<CR>",     -- Start selection
+        node_incremental = "<CR>",   -- Expand to next node
+        scope_incremental = "<Tab>", -- Expand to next scope
+        node_decremental = "<BS>",   -- Shrink selection
+      },
+    },
+  },
+}
+
+return M
