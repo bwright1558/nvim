756adc66313c2e4298cc82b2c75c797ac14ca78e
Brian Wright <bw@shadowdragon.io>
2025-06-26 22:25:19 -0500

    tweak illuminate options

--- lua/plugins/illuminate.lua
+++ lua/plugins/illuminate.lua
@@ -8,7 +8,25 @@
 local M = {
   "RRethy/vim-illuminate",
   event = { "CursorHold", "CursorHoldI" },
-  opts = {},
+  opts = {
+    filetypes_denylist = {
+      "dirbuf",
+      "dirvish",
+      "fugitive",
+      "alpha",
+      "NvimTree",
+      "lazy",
+      "neogitstatus",
+      "Trouble",
+      "lir",
+      "Outline",
+      "spectre_panel",
+      "toggleterm",
+      "DressingSelect",
+      "TelescopePrompt",
+    },
+    large_file_cutoff = 100000,
+  },
   config = function(_, opts)
     require("illuminate").configure(opts)
   end,
