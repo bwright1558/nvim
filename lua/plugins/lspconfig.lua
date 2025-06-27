6aeb0c6bec48d25c15dcafa1e4846ab0e59ec577
Brian Wright <bw@shadowdragon.io>
2025-06-26 21:32:25 -0500

    Cleaning up lspconfig

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,16 +1,17 @@
+-- lspconfig.lua
+--
+-- Automatically installs, configures, enables, and
+-- starts language servers.
+--
+-- See: https://github.com/mason-org/mason-lspconfig.nvim
+--      https://github.com/mason-org/mason.nvim
+--      https://github.com/neovim/nvim-lspconfig
+
 local M = {
   "mason-org/mason-lspconfig.nvim",
   dependencies = {
     "neovim/nvim-lspconfig",
-    {
-      "mason-org/mason.nvim",
-      -- build = ":MasonUpdate",
-      opts = {
-        ui = {
-          border = "rounded",
-        },
-      },
-    },
+    { "mason-org/mason.nvim", opts = { ui = { border = "rounded" } } },
   },
   opts = {
     ensure_installed = {
