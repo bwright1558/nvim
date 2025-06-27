43807c6e8193b3b0c7d06df01ee150151a7bc6be
Brian Wright <bw@shadowdragon.io>
2025-06-27 10:34:05 -0500

    Add lualine plugin

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,71 @@
+-- lualine.lua
+--
+-- A pretty status line for neovim.
+--
+-- See: https://github.com/nvim-lualine/lualine.nvim
+
+local M = {
+  "nvim-lualine/lualine.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = {
+    options = {
+      globalstatus = vim.opt.laststatus == 3,
+      disabled_filetypes = {
+        statusline = { "NvimTree", "Outline" },
+      },
+      component_separators = { left = "", right = "" },
+      section_separators = { left = " ", right = " " },
+    },
+    sections = {
+      lualine_a = { "mode" },
+      lualine_b = {
+        "branch",
+        {
+          "diff",
+          symbols = {
+            added = " ",
+            modified = " ",
+            removed = " ",
+          },
+          source = function()
+            local gitsigns = vim.b.gitsigns_status_dict
+            return gitsigns and {
+              added = gitsigns.added,
+              modified = gitsigns.changed,
+              removed = gitsigns.removed,
+            } or {}
+          end,
+        },
+        {
+          "diagnostics",
+          symbols = {
+            error = " ",
+            warn = " ",
+            info = " ",
+            hint = " ",
+          },
+        },
+      },
+      lualine_c = { "filename" },
+      lualine_x = { "encoding", "fileformat", "filetype" },
+      lualine_y = { "progress", "location" },
+      lualine_z = {
+        function()
+          return " " .. os.date("%I:%M %p")
+        end,
+      },
+    },
+    inactive_sections = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = { "filename" },
+      lualine_x = { "encoding", "fileformat", "filetype", "progress", "location" },
+      lualine_y = {},
+      lualine_z = {},
+    },
+  },
+}
+
+return M
