11ea04cbc9612cd811d03ccaa702f21979c6c0f5
Brian Wright <bw@shadowdragon.io>
2025-06-28 16:38:43 -0500

    add comments to vim_cool plugin config

--- lua/plugins/vim_cool.lua
+++ lua/plugins/vim_cool.lua
@@ -1,3 +1,10 @@
+-- vim_cool.lua
+--
+-- Disables search highlighting when you are done searching
+-- and re-enables it when you search again.
+--
+-- See: https://github.com/romainl/vim-cool
+
 local M = {
   "romainl/vim-cool",
 }
