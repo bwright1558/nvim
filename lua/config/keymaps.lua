f545f5620d6af3e7ec1a124d0594a944bb01c346
Brian Wright <bw@shadowdragon.io>
2025-06-29 12:38:54 -0500

    save progress on new keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -58,3 +58,58 @@ map("t", "<C-l>", [[<C-\><C-n><C-w>l]], { desc = "Terminal window right", silent
 -------------------------------------------------------------------------------
 map("x", "p", 'p:let @+=@0<CR>:let @"=@0<CR>', { desc = "Paste without yanking", silent = true })
 map("x", "P", 'P:let @+=@0<CR>:let @"=@0<CR>', { desc = "Paste without yanking", silent = true })
+
+-------------------------------------------------------------------------------
+-- <Leader> mappings
+-------------------------------------------------------------------------------
+
+-- Toggle Quickfix window
+map("n", "<Leader>,", function()
+  local is_open = false
+  for _, win in ipairs(vim.fn.getwininfo()) do
+    if win.quickfix == 1 then
+      is_open = true
+      break
+    end
+  end
+  vim.cmd(is_open and "cclose" or "copen")
+end, { desc = "Toggle Quickfix", silent = true })
+
+-- Basic operations
+map("n", "<Leader>q", "<Cmd>q<CR>", { desc = "Quit", silent = true })
+map("n", "<Leader>Q", "<Cmd>qall<CR>", { desc = "Quit All", silent = true })
+map("n", "<Leader>w", "<Cmd>w<CR>", { desc = "Save", silent = true })
+map("n", "<Leader>h", "<Cmd>nohlsearch<CR>", { desc = "Clear Search Highlight", silent = true })
+
+-- Comments
+map("n", "<Leader>/", "gcc", { desc = "Toggle Comment Line", silent = true, remap = true })
+map("x", "<Leader>/", "gc", { desc = "Toggle Comment Block", silent = true, remap = true })
+
+-- Buffer navigation
+map("n", "<Leader>bd", "<Cmd>bd<CR>", { desc = "Delete Buffer", silent = true })
+map("n", "<Leader>bj", "<Cmd>bj<CR>", { desc = "Next Buffer", silent = true })
+map("n", "<Leader>bk", "<Cmd>bp<CR>", { desc = "Prev Buffer", silent = true })
+map("n", "<Leader>bl", "<Cmd>b#<CR>", { desc = "Last Buffer", silent = true })
+map("n", "<Leader>bb", "<Cmd>Telescope buffers<CR>", { desc = "Buffer List", silent = true })
+
+-- Quickfix
+map("n", "<Leader>sq", "<Cmd>Telescope quickfix<CR>", { desc = "Quickfix List", silent = true })
+
+-- Git: Fugitive
+map("n", "<Leader>gg", "<Cmd>G<CR>", { desc = "Git Status (Fugitive)", silent = true })
+map("n", "<Leader>g;", "<Cmd>Git push<CR>", { desc = "Git Push", silent = true })
+
+-- Git: Gitsigns
+map("n", "<Leader>gj", function() require("gitsigns").nav_hunk("next") end, { desc = "Next Hunk", silent = true })
+map("n", "<Leader>gk", function() require("gitsigns").nav_hunk("prev") end, { desc = "Prev Hunk", silent = true })
+map("n", "<Leader>gl", function() require("gitsigns").blame_line() end, { desc = "Blame Line", silent = true })
+map("n", "<Leader>gp", function() require("gitsigns").preview_hunk() end, { desc = "Preview Hunk", silent = true })
+map("n", "<Leader>gr", function() require("gitsigns").reset_hunk() end, { desc = "Reset Hunk", silent = true })
+map("n", "<Leader>gR", function() require("gitsigns").reset_buffer() end, { desc = "Reset Buffer", silent = true })
+map("n", "<Leader>gs", function() require("gitsigns").stage_hunk() end, { desc = "Stage/Unstage Hunk", silent = true })
+
+-- Git: Telescope
+map("n", "<Leader>go", "<Cmd>Telescope git_status<CR>", { desc = "Git Status (Telescope)", silent = true })
+map("n", "<Leader>gb", "<Cmd>Telescope git_branches<CR>", { desc = "Git Branches", silent = true })
+map("n", "<Leader>gc", "<Cmd>Telescope git_commits<CR>", { desc = "Git Commits", silent = true })
+map("n", "<Leader>gC", "<Cmd>Telescope git_bcommits<CR>", { desc = "File Commits", silent = true })
--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -35,18 +35,37 @@ vim.diagnostic.config({
 vim.api.nvim_create_autocmd("LspAttach", {
   group = vim.api.nvim_create_augroup("user_config_lsp_attach", { clear = true }),
   callback = function(event)
-    local map = function(keys, func, desc)
-      vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+    local map = function(mode, keys, func, desc)
+      vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc, silent = true })
     end
 
-    map("K", function() vim.lsp.buf.hover({ border = "rounded" }) end, "Show Hover")
-    map("gd", vim.lsp.buf.definition, "Goto Definition")
-    map("gD", vim.lsp.buf.declaration, "Goto Declaration")
-    map("gr", vim.lsp.buf.references, "Goto References")
-    map("gI", vim.lsp.buf.implementation, "Goto Implementation")
-    map("gK", function() vim.lsp.buf.signature_help({ border = "rounded" }) end, "Show Signature Help")
-    map("gl", vim.diagnostic.open_float, "Show Line Diagnostics")
-    map("]d", function() vim.diagnostic.jump({ count = 1, float = true }) end, "Next Diagnostic")
-    map("[d", function() vim.diagnostic.jump({ count = -1, float = true }) end, "Prev Diagnostic")
+    -- Global mappings
+    map("n", "K", function() vim.lsp.buf.hover({ border = "rounded" }) end, "Hover Documentation")
+    map("n", "gK", function() vim.lsp.buf.signature_help({ border = "rounded" }) end, "Signature Help")
+    map("n", "gd", vim.lsp.buf.definition, "Goto Definition")
+    map("n", "gD", vim.lsp.buf.declaration, "Goto Declaration")
+    map("n", "gi", vim.lsp.buf.implementation, "Goto Implementation")
+    -- map("n", "gr", vim.lsp.buf.references, "Goto References")
+    map("n", "gr", "<Cmd>Telescope lsp_references<CR>", "References")
+    map("n", "gl", vim.diagnostic.open_float, "Show Line Diagnostics")
+    map("n", "]d", function() vim.diagnostic.jump({ count = 1, float = true }) end, "Next Diagnostic")
+    map("n", "[d", function() vim.diagnostic.jump({ count = -1, float = true }) end, "Prev Diagnostic")
+
+    -- <Leader> mappings
+    map("n", "<Leader>la", vim.lsp.buf.code_action, "Code Action")
+    map("x", "<Leader>la", vim.lsp.buf.code_action, "Code Action (Visual)")
+    map("n", "<Leader>lf", vim.lsp.buf.format, "Format")
+    map("n", "<Leader>lr", vim.lsp.buf.rename, "Rename Symbol")
+    map("n", "<Leader>lj", function() vim.diagnostic.jump({ count = 1, float = true }) end, "Next Diagnostic")
+    map("n", "<Leader>lk", function() vim.diagnostic.jump({ count = -1, float = true }) end, "Prev Diagnostic")
+    map("n", "<Leader>ll", vim.lsp.codelens.run, "Run CodeLens")
+    map("n", "<Leader>lq", vim.diagnostic.setloclist, "Diagnostics → Loclist")
+    map("n", "<Leader>ld", "<Cmd>Telescope diagnostics bufnr=0<CR>", "Buffer Diagnostics")
+    map("n", "<Leader>lw", "<Cmd>Telescope diagnostics<CR>", "Workspace Diagnostics")
+    map("n", "<Leader>ls", "<Cmd>Telescope lsp_document_symbols<CR>", "Document Symbols")
+    map("n", "<Leader>lS", "<Cmd>Telescope lsp_dynamic_workspace_symbols<CR>", "Workspace Symbols")
+    map("n", "<Leader>li", "<Cmd>LspInfo<CR>", "LSP Info")
+    map("n", "<Leader>lR", "<Cmd>LspRestart<CR>", "Restart LSP")
+    map("n", "<Leader>lI", "<Cmd>Mason<CR>", "Open Mason")
   end,
 })
