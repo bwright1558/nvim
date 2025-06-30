1726b9d72796b60801bc9e6086e6355a66e83f27
Brian Wright <bw@shadowdragon.io>
2025-06-29 19:04:44 -0500

    customize telescope mappings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,41 +12,92 @@ local M = {
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
   },
   cmd = "Telescope",
-  opts = {
-    defaults = {
-      prompt_prefix = " ",
-      selection_caret = " ",
-      path_display = { "smart", "filename_first" },
-      layout_strategy = "flex",
+  opts = function()
+    local actions = require("telescope.actions")
+    return {
+      defaults = {
+        prompt_prefix = " ",
+        selection_caret = " ",
+        path_display = { "smart", "filename_first" },
+        layout_strategy = "flex",
 
-      -- Override live_grep command to include hidden files and exclude .git
-      vimgrep_arguments = {
-        "rg",
-        "--color=never",
-        "--no-heading",
-        "--with-filename",
-        "--line-number",
-        "--column",
-        "--smart-case",
-        "--hidden",
-        "--glob=!**/.git/*",
+        -- Override live_grep command to include hidden files and exclude .git
+        vimgrep_arguments = {
+          "rg",
+          "--color=never",
+          "--no-heading",
+          "--with-filename",
+          "--line-number",
+          "--column",
+          "--smart-case",
+          "--hidden",
+          "--glob=!**/.git/*",
+        },
+        mappings = {
+          i = {
+            ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+            ["<Down>"] = actions.cycle_history_next,
+            ["<Up>"] = actions.cycle_history_prev,
+          },
+          n = {
+            ["<C-n>"] = actions.move_selection_next,
+            ["<C-p>"] = actions.move_selection_previous,
+            ["<Down>"] = actions.cycle_history_next,
+            ["<Up>"] = actions.cycle_history_prev,
+          },
+        },
       },
-    },
-    pickers = {
-      find_files = {
-        -- Override find_files command to include hidden files and exclude .git
-        find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+      pickers = {
+        find_files = {
+          -- Override find_files command to include hidden files and exclude .git
+          find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+        },
       },
-    },
-    extensions = {
-      fzf = {
-        fuzzy = true,
-        override_generic_sorter = true,
-        override_file_sorter = true,
-        case_mode = "smart_case",
+      extensions = {
+        fzf = {
+          fuzzy = true,
+          override_generic_sorter = true,
+          override_file_sorter = true,
+          case_mode = "smart_case",
+        },
       },
-    },
-  },
+    }
+  end,
+  -- opts = {
+  --   defaults = {
+  --     prompt_prefix = " ",
+  --     selection_caret = " ",
+  --     path_display = { "smart", "filename_first" },
+  --     layout_strategy = "flex",
+  --
+  --     -- Override live_grep command to include hidden files and exclude .git
+  --     vimgrep_arguments = {
+  --       "rg",
+  --       "--color=never",
+  --       "--no-heading",
+  --       "--with-filename",
+  --       "--line-number",
+  --       "--column",
+  --       "--smart-case",
+  --       "--hidden",
+  --       "--glob=!**/.git/*",
+  --     },
+  --   },
+  --   pickers = {
+  --     find_files = {
+  --       -- Override find_files command to include hidden files and exclude .git
+  --       find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+  --     },
+  --   },
+  --   extensions = {
+  --     fzf = {
+  --       fuzzy = true,
+  --       override_generic_sorter = true,
+  --       override_file_sorter = true,
+  --       case_mode = "smart_case",
+  --     },
+  --   },
+  -- },
   config = function(_, opts)
     local telescope = require("telescope")
     telescope.setup(opts)
