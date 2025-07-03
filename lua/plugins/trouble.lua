f089e2375ba6f346d1b31be6f495eaa88abdaad2
Brian Wright <bw@shadowdragon.io>
2025-07-02 23:08:15 -0500

    Add trouble.nvim and persistence.nvim plugins

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,14 @@
+-- trouble.lua
+--
+-- A pretty diagnostics, references, telescope results, quickfix and
+-- location list to help you solve all the trouble your code is causing.
+--
+-- See: https://github.com/folke/trouble.nvim
+
+local M = {
+  "folke/trouble.nvim",
+  cmd = "Trouble",
+  opts = {},
+}
+
+return M
