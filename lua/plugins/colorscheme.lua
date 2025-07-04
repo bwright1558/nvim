a1dbeaebdb02fa910672156919ebe86a97f540ae
Brian Wright <bw@shadowdragon.io>
2025-07-03 20:58:46 -0500

    be explicit about colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -8,7 +8,11 @@ local M = {
   "catppuccin/nvim",
   name = "catppuccin",
   priority = 1000,
-  config = function()
+  opts = {
+    flavour = "mocha",
+  },
+  config = function(_, opts)
+    require("catppuccin").setup(opts)
     vim.cmd.colorscheme("catppuccin")
   end,
 }
