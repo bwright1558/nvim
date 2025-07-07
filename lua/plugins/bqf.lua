8045a7349efc3703a048280f2a604fabc02063dc
Brian Wright <bw@shadowdragon.io>
2025-07-07 11:45:44 -0500

    add bqf plugin for quickfix preview

---
+++ lua/plugins/bqf.lua
@@ -0,0 +1,13 @@
+-- bqf.lua
+--
+-- Adds a preview window for items in the quickfix window.
+--
+-- See: https://github.com/kevinhwang91/nvim-bqf
+
+local M = {
+  "kevinhwang91/nvim-bqf",
+  ft = "qf",
+  opts = {},
+}
+
+return M
