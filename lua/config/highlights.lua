dacaf96aba5b30368a3e4905cd6d8eb2edb6a88c
Brian Wright <bw@shadowdragon.io>
2025-07-09 10:54:07 -0500

    transparent background :)

---
+++ lua/config/highlights.lua
@@ -0,0 +1,9 @@
+local function set_transparent(name)
+  vim.api.nvim_set_hl(0, name, { bg = "NONE" })
+end
+
+-- Transparent UI
+set_transparent("Normal")
+set_transparent("NormalNC")
+-- set_transparent("NormalFloat")
+-- set_transparent("FloatBorder")
