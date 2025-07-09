63c1734f8a7c1394b332a73cc8448f644965ea02
Brian Wright <bw@shadowdragon.io>
2025-07-09 10:24:13 -0500

    tweak oil.nvim keymaps to not conflict with my keymaps

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -14,6 +14,14 @@ local M = {
     view_options = {
       show_hidden = true,
     },
+    keymaps = {
+      ["<C-s>"] = false,             -- default is "actions.select" veritical split
+      ["<C-h>"] = false,             -- default is "actions.select" horizontal split
+      ["<C-j>"] = false,             -- no default
+      ["<C-k>"] = false,             -- no default
+      ["<C-l>"] = false,             -- default is "actions.refresh"
+      ["<C-r>"] = "actions.refresh", -- no default
+    },
   },
 }
 
