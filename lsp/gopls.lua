bf7ba4d39d1471aae44abfaa643a7380b33d6291
Brian Wright <bw@shadowdragon.io>
2025-07-22 11:01:54 -0500

    add a comment about the gopls settings

--- lsp/gopls.lua
+++ lsp/gopls.lua
@@ -2,6 +2,8 @@ return {
   settings = {
     gopls = {
       env = {
+        -- Fixes the "watch.watch: ENOENT: no such file or directory" error
+        -- caused by a go.work file being present in a project.
         GOWORK = "off",
       },
     },
