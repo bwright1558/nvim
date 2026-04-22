a9c6ddc6b879f508477380fd93bebf8d4f4b91fa
Brian Wright <bw@shadowdragon.io>
2026-04-22 00:05:55 -0500

    re-enable markdown rendering

---
+++ lua/plugins/render_markdown.lua
@@ -0,0 +1,23 @@
+-- render_markdown.lua
+--
+-- Improve viewing Markdown files in neovim.
+--
+-- See: https://github.com/MeanderingProgrammer/render-markdown.nvim
+
+local M = {
+  "MeanderingProgrammer/render-markdown.nvim",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
+  },
+  ft = "markdown",
+  opts = {
+    latex = { enabled = false },
+    completions = {
+      lsp = { enabled = true },
+      blink = { enabled = true },
+    },
+  },
+}
+
+return M
