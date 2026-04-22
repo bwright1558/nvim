52780b3e2ceefd0c8fce43f693a4f9f5fa68dc2f
Brian Wright <bw@shadowdragon.io>
2026-04-21 23:18:10 -0500

    enable treesitter indent

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -158,7 +158,7 @@ local M = {
         end
 
         pcall(vim.treesitter.start)
-        -- vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
       end,
     })
   end,
